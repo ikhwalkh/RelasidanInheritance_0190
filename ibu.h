a2b4ea3d7a2692d053b239e09d0b6ab1d052f1f4
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-11 15:06:42 +0700

    class ibu

--- ibu.h
+++ ibu.h
@@ -3,5 +3,16 @@
 #include <vektor>
 
 class ibu {
-    
-}
+    public:
+    string nama;
+    vektor<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};

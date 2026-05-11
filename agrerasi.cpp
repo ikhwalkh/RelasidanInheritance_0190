77a7f939def48c86fd3a8b3f9437bdc38bee4476
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-11 15:53:36 +0700

    agregasi

--- agrerasi.cpp
+++ agrerasi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main() {
+int main() { //agregasi
     ibu* varIbu = new ibu("dini");
     ibu* varibu2 = new ibu("novi");
     anak* varAnak1 = new anak("tono");
